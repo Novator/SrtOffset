59aa39cd9c0a922c546978d9e195c127106fa6d9
Michael Galyuk <robux@mail.ru>
2022-12-24 09:35:48 +0500

    version 0.01

---
+++ srtoffset.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#python3 srtoffset.py ./subt.srt '00:01:06,500' > ./subt1.srt
+#python3 srtoffset.py ./subt.srt '00:00:21,300' > ./subt2.srt
+python3 srtoffset.py ./subt.srt '00:01:57,500' > ./subt1.1.srt
+
